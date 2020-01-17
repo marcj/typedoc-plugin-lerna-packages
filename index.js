9323a0c8714380bbedeac458fb8ecf74b5ed5f90
Gerrit Birkeland <gerrit@gerritbirkeland.com>
2020-01-17 15:52:13 -0700

    feat: Support for TypeDoc v0.16

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var plugin = require("./plugin");
-
-module.exports = function (PluginHost) {
-    var app = PluginHost.owner;
-
-    app.options.addDeclaration({name: 'lerna-packages-exclude', short: 'lpe'});
-
-    app.converter.addComponent('lerna-packages', plugin.LernaPackagesPlugin);
-};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const plugin_1 = require("./plugin");
+const typedoc_1 = require("typedoc");
+function load(PluginHost) {
+    const app = PluginHost.owner;
+    app.options.addDeclaration({
+        name: 'lernaExclude',
+        help: 'List of package names that should be excluded.',
+        type: typedoc_1.ParameterType.Array
+    });
+    app.options.addDeclaration({
+        name: 'pathExclude',
+        help: 'List of paths to entirely ignore',
+        type: typedoc_1.ParameterType.Array,
+        defaultValue: []
+    });
+    app.converter.addComponent('lerna-packages', plugin_1.LernaPackagesPlugin);
+}
+exports.load = load;
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,22 @@
+import { LernaPackagesPlugin } from './plugin'
+import { PluginHost } from 'typedoc/dist/lib/utils';
+import { ParameterType } from 'typedoc';
+
+export function load(PluginHost: PluginHost) {
+    const app = PluginHost.owner;
+
+    app.options.addDeclaration({
+        name: 'lernaExclude',
+        help: 'List of package names that should be excluded.',
+        type: ParameterType.Array
+    });
+
+    app.options.addDeclaration({
+        name: 'pathExclude',
+        help: 'List of paths to entirely ignore',
+        type: ParameterType.Array,
+        defaultValue: []
+    });
+
+    app.converter.addComponent('lerna-packages', LernaPackagesPlugin);
+}
